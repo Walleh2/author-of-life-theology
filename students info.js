684a3adac5938b90a0618fcba08fdcf9f1ad6502
Walleh2 <128179422+Walleh2@users.noreply.github.com>
2026-05-26 16:34:25 +0100

    Add files via upload

---
+++ students info.js
@@ -0,0 +1,4 @@
+// Example structural strategy for loading active profile data
+document.getElementById('student-name-field').innerText = dynamicStudentData.fullName;
+document.getElementById('student-id-field').innerText = dynamicStudentData.enrollmentId;
+document.getElementById('count-courses').innerText = dynamicStudentData.totalCourses;
